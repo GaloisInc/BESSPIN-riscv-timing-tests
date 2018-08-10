45e2ac8d83ccbadbc30b3b0c0f5045f52b8699ba
Ashay Rane <arane@galois.com>
2018-08-10 08:06:56 -0700

    Making outer loop concurrent, effecitively permitting greater benefits from concurrency.

--- scripts/latency-test.go
+++ scripts/latency-test.go
@@ -20,37 +20,16 @@ var ROCKET_DIR = bagpipe.HomeDirectory() + "/src/rocket-chip/emulator"
 var ROCKET_BIN = "./emulator-freechips.rocketchip.system-DefaultConfig"
 
 type input_t struct {
-	Binary_path  string
+	Noop_count   string
 	Emulator_dir string
 	Emulator_bin string
 }
 
 type output_t struct {
-	Instr_count string
-	Cycle_count string
-}
-
-func exec(emulator_dir string, emulator_bin string, exe_file string) string {
-	if bagpipe.FileExists(exe_file) == false {
-		panic("could not find " + exe_file)
-	}
-
-	cmd := emulator_bin + " -s 0 " + exe_file
-	return bagpipe.ExecCommand(cmd, emulator_dir)
-}
-
-func __exec_one(input input_t) output_t {
-	exec_output := exec(input.Emulator_dir, input.Emulator_bin,
-		input.Binary_path)
-
-	instrs, cycles := parse(exec_output)
-	return output_t{Instr_count: instrs, Cycle_count: cycles}
-}
-
-func exec_one(input bytes.Buffer) bytes.Buffer {
-	__input := decode_input(input)
-	__output := __exec_one(__input)
-	return encode_output(__output)
+	Min_instr_count string
+	Max_instr_count string
+	Min_cycle_count string
+	Max_cycle_count string
 }
 
 func encode_input(value input_t) bytes.Buffer {
@@ -114,7 +93,16 @@ func parse(line string) (string, string) {
 	return instr_count, cycle_count
 }
 
-func kernel(noop_count int, repetitions int) (uint64, uint64, uint64, uint64) {
+func exec(emulator_dir string, emulator_bin string, exe_file string) string {
+	if bagpipe.FileExists(exe_file) == false {
+		panic("could not find " + exe_file)
+	}
+
+	cmd := emulator_bin + " -s 0 " + exe_file
+	return bagpipe.ExecCommand(cmd, emulator_dir)
+}
+
+func kernel(noop_count uint64, em_dir string, em_bin string) (uint64, uint64, uint64, uint64) {
 	exe_file := bagpipe.CreateTempFile("latency-test.")
 
 	cmd := fmt.Sprintf("riscv64-unknown-elf-gcc -Ienv -Icommon "+
@@ -131,23 +119,13 @@ func kernel(noop_count int, repetitions int) (uint64, uint64, uint64, uint64) {
 	min_instrs := uint64(math.MaxUint64)
 	min_cycles := uint64(math.MaxUint64)
 
-	sprinter := bagpipe.NewSprinter(exec_one, MAX_THREADS, repetitions)
+	for idx := 0; idx < 3; idx += 1 {
+		__instrs, __cycles := parse(exec(em_dir, em_bin, exe_file))
 
-	for idx := 0; idx < repetitions; idx += 1 {
-		input := input_t{Binary_path: exe_file, Emulator_dir: ROCKET_DIR,
-			Emulator_bin: ROCKET_BIN}
-
-		sprinter.FeedWorker(input)
-	}
-
-	for idx := 0; idx < sprinter.ResultCount(); idx += 1 {
-		_, __output := sprinter.ReadResult()
-		output := decode_output(__output)
-
-		instrs, err := strconv.ParseUint(output.Instr_count, 16, 64)
+		instrs, err := strconv.ParseUint(__instrs, 16, 64)
 		bagpipe.CheckError(err)
 
-		cycles, err := strconv.ParseUint(output.Cycle_count, 16, 64)
+		cycles, err := strconv.ParseUint(__cycles, 16, 64)
 		bagpipe.CheckError(err)
 
 		if instrs < min_instrs {
@@ -171,11 +149,73 @@ func kernel(noop_count int, repetitions int) (uint64, uint64, uint64, uint64) {
 	return min_instrs, max_instrs, min_cycles, max_cycles
 }
 
-func main() {
-	for noop_count := 0; noop_count < 1024; noop_count += 1 {
-		min_instrs, max_instrs, min_cycles, max_cycles := kernel(noop_count, 4)
+func __exec_one(input input_t) output_t {
+	noop_count, err := strconv.ParseUint(input.Noop_count, 16, 64)
+	bagpipe.CheckError(err)
 
-		fmt.Printf("noops: %4d\tinstrs: [ %d - %d ]\tcycles: [ %d - %d ]\n",
-			noop_count, min_instrs, max_instrs, min_cycles, max_cycles)
+	min_instrs, max_instrs, min_cycles, max_cycles := kernel(noop_count,
+		input.Emulator_dir, input.Emulator_bin)
+
+	s_min_instrs := strconv.FormatUint(min_instrs, 16)
+	s_min_cycles := strconv.FormatUint(min_cycles, 16)
+	s_max_instrs := strconv.FormatUint(max_instrs, 16)
+	s_max_cycles := strconv.FormatUint(max_cycles, 16)
+
+	return output_t{Min_instr_count: s_min_instrs,
+		Max_instr_count: s_max_instrs, Min_cycle_count: s_min_cycles,
+		Max_cycle_count: s_max_cycles}
+}
+
+func exec_one(input bytes.Buffer) bytes.Buffer {
+	__input := decode_input(input)
+	__output := __exec_one(__input)
+	return encode_output(__output)
+}
+
+func main() {
+	max_noops := 1024
+	sprinter := bagpipe.NewSprinter(exec_one, MAX_THREADS, max_noops)
+
+	for noop_count := uint64(0); noop_count < uint64(max_noops); noop_count += 1 {
+		s_noop_count := strconv.FormatUint(noop_count, 16)
+
+		input := input_t{Noop_count: s_noop_count, Emulator_dir: BOOM_DIR,
+			Emulator_bin: BOOM_BIN}
+
+		status := fmt.Sprintf("%4d of %4d", noop_count+1, max_noops)
+		bagpipe.UpdateStatus("testing [" + status + " ] ... ")
+
+		sprinter.FeedWorker(input)
 	}
+
+	bagpipe.ClearStatus()
+
+	for idx := 0; idx < sprinter.ResultCount(); idx += 1 {
+		__input, __output := sprinter.ReadResult()
+
+		input := decode_input(__input)
+		output := decode_output(__output)
+
+		noop_count, err := strconv.ParseUint(input.Noop_count, 16, 64)
+		bagpipe.CheckError(err)
+
+		min_instrs, err := strconv.ParseUint(output.Min_instr_count, 16, 64)
+		bagpipe.CheckError(err)
+
+		max_instrs, err := strconv.ParseUint(output.Max_instr_count, 16, 64)
+		bagpipe.CheckError(err)
+
+		min_cycles, err := strconv.ParseUint(output.Min_cycle_count, 16, 64)
+		bagpipe.CheckError(err)
+
+		max_cycles, err := strconv.ParseUint(output.Max_cycle_count, 16, 64)
+		bagpipe.CheckError(err)
+
+		log_line := fmt.Sprintf("noops: %4d\tinstrs: [ %d - %d ]\tcycles: "+
+			"[ %d - %d ]\n", noop_count, min_instrs, max_instrs, min_cycles,
+			max_cycles)
+		bagpipe.AppendFile("latency-results.txt", log_line)
+	}
+
+	bagpipe.UpdateStatus("finished, results in latency-results.txt\n")
 }
