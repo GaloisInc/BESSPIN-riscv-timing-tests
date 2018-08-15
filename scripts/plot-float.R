a826f76d9cb6afa1bb789b7a4faac7cbccde2e09
Ashay Rane <arane@galois.com>
2018-08-15 16:06:16 -0700

    Minor: using vector graphics to plot heat maps.

--- scripts/plot-float.R
+++ scripts/plot-float.R
@@ -19,10 +19,10 @@ data.melt <- melt(data.mat)
 
 zp1 <- ggplot(data.melt, aes(x = Var1, y = Var2, fill = value))
 zp1 <- zp1 + labs(title = "latency (ratio)", x = "Operand #1", y = "Operand #2")
-zp1 <- zp1 + theme_classic() + theme(line = element_blank(), axis.title = element_text(size = 5), axis.text = element_text(size = 5, hjust = 1), plot.title = element_text(size = 6, hjust = 0.5), axis.text.x = element_text(angle = 90, hjust = 1))
+zp1 <- zp1 + theme_classic() + theme(line = element_blank(), axis.title = element_text(size = 10), axis.text = element_text(size = 10, hjust = 1), plot.title = element_text(size = 12, hjust = 0.5), axis.text.x = element_text(angle = 90, hjust = 1))
 zp1 <- zp1 + geom_tile()
 zp1 <- zp1 + scale_fill_gradient2(high = "#F15946", guide = FALSE)
-zp1 <- zp1 + geom_text(aes(Var1, Var2, label = round(value, 1)), size = 0.7)
+zp1 <- zp1 + geom_text(aes(Var1, Var2, label = round(value, 1)), size = 3)
 zp1 <- zp1 + coord_equal()
 
-ggsave(paste("plot.png", sep = ""), width = 2, height = 2, dpi = 420)
+ggsave("plot.pdf", width = 4, height = 4, dpi = 420)
