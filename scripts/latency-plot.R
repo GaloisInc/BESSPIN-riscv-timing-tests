574a63a39e41eea16dc54a3b1c23d8b0f3594215
Ashay Rane <arane@galois.com>
2018-08-15 16:15:24 -0700

    Minor: using vector graphics.

--- scripts/latency-plot.R
+++ scripts/latency-plot.R
@@ -11,20 +11,21 @@ if (length(args) != 1) {
 data <- read.table(args[1], header = F)
 data$V2 <- data$V2 / min(data$V2)
 
-# data <- head(data, 32)
+data <- head(data, 64)
 min_x <- min(data$V1)
 max_x <- max(data$V1)
 
 ggplot(data, aes(V1, V2)) +
-    geom_point(size = 0.05, color = "gray20") +
-    scale_x_continuous(breaks = seq(min_x, max_x, by = 64)) +
+    geom_point(size = 1, color = "red") +
+    scale_x_continuous(breaks = seq(min_x, max_x, by = 16)) +
+    scale_y_continuous(limits = c(0, 40)) +
     theme_classic() +
     theme(line = element_blank(),
         panel.grid.major = element_line(colour = "gray60", size = 0.05),
-        axis.title = element_text(size = 5),
-        axis.text = element_text(size = 5, hjust = 0.5),
-        plot.title = element_text(size = 6, hjust = 0.5)) +
-    xlab("Number of NO-OP Instructions") +
-    ylab("Slowdown")
+        axis.title = element_text(size = 14),
+        axis.text = element_text(size = 14, hjust = 0.5),
+        plot.title = element_text(size = 16, hjust = 0.5)) +
+    xlab("Offset of Start Address of Measurement Code") +
+    ylab("Execution Time (Cycles)")
 
-ggsave("plot.png", width = 6, height = 2)
+ggsave("plot.pdf", width = 9, height = 3)
